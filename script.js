09eaaf6c71d2235024e55ec865f5517904275c20
Kartikagrahari2003 <kartiktheking12345@gmail.com>
2026-07-05 21:55:45 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const button = document.getElementById("btn");
+
+button.addEventListener("click", function () {
+
+    alert("Welcome to GitHub Actions CI/CD Project!");
+
+});
+
+console.log("Website Loaded Successfully");
